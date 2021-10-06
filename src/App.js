743c43cff5b76b640a209437fc2b928b39ff84b4
GalinaSherbak84 <galinasherbak@gmail.com>
2021-10-06 16:13:09 +0200

    add  styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import './App.css';
 import Todo from './components/Todo';
 import Form from './components/Form';
 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,15 @@
 import React from "react";
-
 function Todo(props){
     
     return(
-        <div style = {{textDecoration: props.todo.completato ? 'Line-through': ''}}>
-            {props.todo.name}
-            <div>
-                <button onClick={() => props.completaTodo(props.index)}>completa</button>
+        <div className="todo" >
+            <h3 style = {{textDecoration: props.todo.completato ? 'Line-through': ''}}> {props.todo.name}</h3>
+           
+            <div className="buttons">
+                <button onClick={() => props.completaTodo(props.index)}>{props.todo.completato ? 'ripristina' : 'completa'}</button>
+                <button className="canc" onClick={() => props.cancTodo(props.index)}>elimina</button>
             </div>
-            <div>
-                <button onClick={() => props.cancTodo(props.index)}>elimina</button>
-            </div>
+
         </div>
     )
 }
